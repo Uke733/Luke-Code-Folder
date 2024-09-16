4bad8f5740dd7c509fc0a7689b6a9b90e18d47f3
Uke733 <luke.filipchuk@rcsd.ca>
2024-09-16 08:54:02 -0600

    Testing

---
+++ 1-Lesson and Demos/Lesson4Demo/src/App.java
@@ -0,0 +1,13 @@
+// Project Name: Lesson 4 Demon 
+// Name: Mr.Fox
+// Date: 13 Sept 2024
+// Description: Going over comments and special characters 
+ 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("How is your day going?");
+        System.out.println("It\'s \"cloudy\" with a chance of meatballs");
+    }
+}
