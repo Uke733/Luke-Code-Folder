bc4c5f01d7d5c9140d0c303cd89de984719273c2
Uke733 <luke.filipchuk@rcsd.ca>
2024-09-19 09:51:35 -0600

    sept 19

---
+++ 1-Lesson and Demos/A6 Chicken Wings/src/App.java
@@ -0,0 +1,17 @@
+/*Program name: (Chicken Wing Price Calculator)
+
+Student name: (Luke Filipchuk)
+
+Date: (The date you completed the program)
+
+Program Description: Calculates how many chicken wings you can buy with $4.75.*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        double chicken;
+        chicken = 0.75;
+        double money;
+        money = 4.75;
+        double HowManyChickens;
+        System.out.println("You can buy " +  HowManyChickens(0) + " chicken wings!");
+    }
+}
---
+++ 1-Lesson and Demos/String Demo/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 = "This is a test";
+        String message2 = "Hellos Class";
+
+        System.out.println(message1.length());
+        
+    }
+}
