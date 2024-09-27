c8519096e4b323d5a25ab6daab2a1a7f672b511f
Uke733 <luke.filipchuk@rcsd.ca>
2024-09-27 09:48:57 -0600

    Sept 27

---
+++ 1-Lesson and Demos/A8-b Passing Methods/src/Burger.java
@@ -0,0 +1,13 @@
+/*Program name: (A8-b Passings Methods)
+
+Student name: (Luke Filipchuk)
+
+Date: (Sept 27)
+
+Program Description: Calls from different classes*/
+public class Burger {
+    public void speak() {
+        System.out.println("Whopper");
+       
+    }
+}
---
+++ 1-Lesson and Demos/A8-b Passing Methods/src/BurgerKing.java
@@ -0,0 +1,9 @@
+public class BurgerKing {
+    public static void main(String[] args) throws Exception {
+        Burger whopper = new Burger();
+    public void order () {
+        System.out.println("Burger please, im hungry");
+        speak();
+        }
+
+}
