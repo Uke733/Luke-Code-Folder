78cc4102e5e7be671f21075a552be0079139e7ac
Uke733 <luke.filipchuk@rcsd.ca>
2024-10-02 09:46:12 -0600

    Oct 2

---
+++ 1-Lesson and Demos/A9 Big or Small/src/BigOrSmall_.java
@@ -0,0 +1,17 @@
+/*Program name: (Assignment 8 Big or small)
+
+Student name: (Luke Filipchuk)
+
+Date: (The date you completed the program)
+
+Program Description: check 2 parameters.  If a is > b, output "yes".  If b is >
+a, output "no".  If a is equal to b, return "equals".
+
+Program Description:*/
+import java.util.Scanner;
+
+public class BigOrSmall_ {
+    public static void main(String[] args) throws Exception {
+        public static void 
+    }
+}
