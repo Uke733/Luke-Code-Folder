844d804fbb5e5674de4da847b523a1b3d1dc2f88
Uke733 <luke.filipchuk@rcsd.ca>
2024-10-09 09:36:41 -0600

    Oct 9
    Done Assignment 10

---
+++ 1-Lesson and Demos/A10 Sum Up Stuff/src/Sum.java
@@ -0,0 +1,30 @@
+//Program name: (Assignment 10 Sum up Stuff)
+
+//Student name: (Luke Filipchuk)
+
+//Date: (Oct 9)
+
+//Program Description: Given two numbers, calculate the sum and print the sum
+public class Sum
+{
+	private double one, two, ans;
+
+	public void setNums(double A, double B)
+	{
+		one = A;
+		two = B;
+	}
+
+	public void sum( )
+	{
+		ans = (one + two); 
+	}
+
+	public void print( )
+	{
+		System.out.println(ans);
+	}
+}
+
+
+	
---
+++ 1-Lesson and Demos/A10 Sum Up Stuff/src/SumRunner.java
@@ -0,0 +1,14 @@
+public class SumRunner
+{
+	public static void main( String[] args )
+	{
+		Sum test = new Sum();				
+		test.setNums(2.0,6.0);
+		test.sum();
+		test.print();
+		
+			
+	}
+}
+
+
