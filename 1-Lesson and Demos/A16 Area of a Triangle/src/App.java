d2cc57b8e1b26daed39083c7bbf2f5faecb0fc3d
Uke733 <luke.filipchuk@rcsd.ca>
2024-10-22 09:34:18 -0600

    A16 done

--- 1-Lesson and Demos/A16 Area of a Triangle/src/App.java
+++ 1-Lesson and Demos/A16 Area of a Triangle/src/App.java
@@ -2,14 +2,14 @@
 
 //Student name: (Luke Filipchuk)
 
-//Date: (Change)
+//Date: (Oct 22)
 
 //Program Description: Calculate the area of a triangle given the values for all three sides.
 import java.util.Scanner;
 
 public class App {
     private Scanner keyboard = new Scanner(System.in);
-    private double P,S,AreaSetUp,A;
+    private double P,S,A;
     public void SolveS(){
         System.out.print("Enter side length #1: ");
         double len1 = keyboard.nextDouble();
@@ -17,12 +17,11 @@ public class App {
         double len2 = keyboard.nextDouble();
         System.out.print("Enter side length #3: ");
         double len3 = keyboard.nextDouble();
-        
         P = len1 + len2 + len3;
         S = P / 2.0;
-        AreaSetUp = S*(S-len1*(S-len2)*(S-len3));
-        A = Math.sqrt(AreaSetUp);
+        A = Math.sqrt(S*((S-len1)*(S-len2)*(S-len3)));
         //Use print f to include number to a certain amount of digits
-        System.out.print("The Area of this Triangle is: " + System.out.printf(|"%.3f\n", Math.sqrt(AreaSetUp));
+        System.out.printf("%.3f", A);
+        System.out.print(", is the area of this triangle to three decimal places");
     }
 }
