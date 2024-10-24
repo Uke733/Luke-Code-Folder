c8f23fa4ce0ce0157f027c5ba129b29a5896c66e
Uke733 <luke.filipchuk@rcsd.ca>
2024-10-24 09:47:00 -0600

    Oct 24

---
+++ 1-Lesson and Demos/A22 Leap Year/src/App.java
@@ -0,0 +1,17 @@
+//Program name: (Assignment 22 Leap year)
+
+//Student name: (Luke Filipchuk)
+
+//Date: (The date you completed the program)
+
+//Program Description: Write a program to determine if a year is a leap year.
+import java.util.Scanner;
+public class App {
+    private Scanner keyboard = new Scanner(System.in);
+    public void LeapYear(){
+        System.out.println("Enter a year: ");
+        int year = keyboard.nextInt();
+        year = 
+    }
+    }
+
