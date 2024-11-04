9760299545950020ce5169b350aa6694df02c76c
Uke733 <luke.filipchuk@rcsd.ca>
2024-11-04 09:47:02 -0600

    Assignment 23 Cool Numbers Done

--- 1-Lesson and Demos/A23 Cool_Numbers/src/App.java
+++ 1-Lesson and Demos/A23 Cool_Numbers/src/App.java
@@ -2,28 +2,33 @@
 
 //Student name: (Luke Filipchuk)
 
-//Date: (Oct 30)
+//Date: (Nov 4)
 
 //Program Description:Complete methods isCoolNum( n ) && countCoolNumbers( n ).  isCoolNum( n) will determine if a number is a cool number.  A cool number is any number that has a remainder of 1 when divided by 3,4,5, and 6.  countCoolNumbers will count the number
 //of cool numbers between 6 and the parameter passed in.   
-import java.util.Scanner;
+
 public class App {
-        private Scanner keyboard = new Scanner(System.in);
-        public void isCoolNum(){
-            System.out.println("Enter a number: ");
-            int num = keyboard.nextInt();
-            if num % 3 == 1 || num % 4 == 1 || num % 5 == 1 || num % 6 == 1{
-                System.out.print("This is the coolest number.");
+
+        public boolean isCoolNum(int num){ 
+
+            if(num % 3 == 1 && num % 4 == 1 && num % 5 == 1 && num % 6 == 1){
+                return true;
             }
+
             else{
-                System.out.print("This is not a cool number.");
-            }
-            
+                return false;
+                }
         }
-    
-        public void countCoolNumbers(){
-            for (int x = 6, x < num, x++){
-                System.out.print(x);
+        
+        public void countCoolNumbers(int end){
+            int total = 0;
+            for (int x = 6; x < end; x++){
+
+                if (isCoolNum(x)){
+                    total++;                
+                }
+
             }
+            System.out.println(total + " cool numbers between 6 - " + end);
         }
     }
--- 1-Lesson and Demos/A23 Cool_Numbers/src/CoolNumbersRunner.java
+++ 1-Lesson and Demos/A23 Cool_Numbers/src/CoolNumbersRunner.java
@@ -1,3 +1,16 @@
-public class CoolNumbersRunner {
+import java.util.Scanner;
 
+public class CoolNumbersRunner {
+public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        App Luke = new App();
+
+        System.out.print("Enter a number: ");
+        int num = keyboard.nextInt();
+        
+        Luke.countCoolNumbers(num);
+
+
+        keyboard.close();
+}
 }
