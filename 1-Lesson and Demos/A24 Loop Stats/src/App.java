0eeb3f1c366712fa0f174046bf6357a388f22b0f
Uke733 <luke.filipchuk@rcsd.ca>
2024-11-07 09:33:54 -0600

    Nov 7

--- 1-Lesson and Demos/A24 Loop Stats/src/App.java
+++ 1-Lesson and Demos/A24 Loop Stats/src/App.java
@@ -7,9 +7,10 @@
 //Program Description: Run a loop from a provided start number to a provided stop number. 
 //Total up all numbers in the group, determine all odds in the group, and determine all evens in the group.
 public class App {
-
-        public void Count(int num, int end){
-            int total, total2;
+public int total, total2;
+App Luke = new App();        
+public void Count(int num, int end){
+            System.out.println(total);;
             for (int x=num; x < end; x++){
                 if (num % 2 == 0){
                     total++;
@@ -21,6 +22,6 @@ public class App {
 
             }
         }
-        }
+}
     
 
