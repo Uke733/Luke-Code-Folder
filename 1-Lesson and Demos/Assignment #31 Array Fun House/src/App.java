156d5f3947020c0b7c49411b16416a5855113374
Uke733 <luke.filipchuk@rcsd.ca>
2024-11-22 09:48:41 -0600

    Nov 22
    Arrays

---
+++ 1-Lesson and Demos/Assignment #31 Array Fun House/src/App.java
@@ -0,0 +1,18 @@
+//Program name: (Assignment 31 Array Fun House)
+
+//Student name: (Luke Filipchuk)
+
+//Date: (Nov 22)
+
+//Program Description: Write several array manipulation methods. 
+//One method will sum up a section of a provided array, another 
+//method will count up how many of a certain number occur in the 
+//array, and the last method will remove all of a certain value from the array.
+import java.util.Random;
+import java.util.Arrays;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] array = {7,4,10,0,1,7,6,5,3,2,9,7}
+
+    }
+}
