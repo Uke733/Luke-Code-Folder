c7fd10dbc64b43f52e1a17f13fa1a255f4eb9c44
Uke733 <luke.filipchuk@rcsd.ca>
2024-11-29 09:48:44 -0600

    Nov 29

--- 1-Lesson and Demos/Assignment #31 Array Fun House/src/App.java
+++ 1-Lesson and Demos/Assignment #31 Array Fun House/src/App.java
@@ -2,25 +2,29 @@
 
 //Student name: (Luke Filipchuk)
 
-//Date: (Nov 22)
+//Date: (Nov 29)
 
 //Program Description: Write several array manipulation methods. 
 //One method will sum up a section of a provided array, another 
 //method will count up how many of a certain number occur in the 
 //array, and the last method will remove all of a certain value from the array.
 //import java.util.Scanner;
-//import java.util.Arrays;
+import java.util.Arrays;
 public class App {
-        //Scanner keys = new Scanner(System.in);
-        private static int PrintSum;
-        public static void AddSum(int[] ArraySet, int start, int end){
-            for(int i = ArraySet[start]; i < end; i = ArraySet[start+1]){
-                PrintSum += i;
+        //Scanner keys = new Scanner(System.in); 
+        public static void AddSum(int[] ArraySet, int start, int end){     
+            Arrays.toString(ArraySet);
+            int PrintSum = 0;
+            for(int i = 0; i < ArraySet.length; i++){
+                if (start <= i && i <= end){
+                    PrintSum += ArraySet[i];
+                }
             }
-            System.out.println("The sum of spots" + start + " - " + end + " = " + PrintSum);
+            System.out.println("The sum of spots " + start + "-" + end + " = " + PrintSum);
         }
     public static void main(String[] args){
         int[] array = {7,4,10,0,1,7,6,5,3,2,9,7};
         AddSum(array,3,6);
+        AddSum(array,2,9);
     }
 }
