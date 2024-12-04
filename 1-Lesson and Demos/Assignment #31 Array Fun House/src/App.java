f05bd815fdf1308554b849ed401a812cfb95720b
Uke733 <luke.filipchuk@rcsd.ca>
2024-12-04 08:42:04 -0600

    Assignment 31 Array Fun House Done

--- 1-Lesson and Demos/Assignment #31 Array Fun House/src/App.java
+++ 1-Lesson and Demos/Assignment #31 Array Fun House/src/App.java
@@ -2,18 +2,20 @@
 
 //Student name: (Luke Filipchuk)
 
-//Date: (Nov 29)
+//Date: (Dec 4)
 
 //Program Description: Write several array manipulation methods. 
 //One method will sum up a section of a provided array, another 
 //method will count up how many of a certain number occur in the 
 //array, and the last method will remove all of a certain value from the array.
-//import java.util.Scanner;
 import java.util.Arrays;
 public class App {
-        //Scanner keys = new Scanner(System.in); 
+        //Prints the array being used
+        public static void PrintSet(int[] ArraySet){
+            System.out.println(Arrays.toString(ArraySet));
+        }
+        //Adds the sum between two numbers in an array
         public static void AddSum(int[] ArraySet, int start, int end){     
-            Arrays.toString(ArraySet);
             int PrintSum = 0;
             for(int i = 0; i < ArraySet.length; i++){
                 if (start <= i && i <= end){
@@ -22,9 +24,23 @@ public class App {
             }
             System.out.println("The sum of spots " + start + "-" + end + " = " + PrintSum);
         }
+        //Counts the number of times that a number occurs in an array
+        public static void Count(int[] ArraySet, int number){
+            int total = 0;
+            for(int i = 0; i < ArraySet.length; i++){
+                if (number == ArraySet[i]){
+                    total++;
+                }
+        }
+        System.out.println("# of  " + number + " 's = " + total);
+    }
     public static void main(String[] args){
         int[] array = {7,4,10,0,1,7,6,5,3,2,9,7};
+        PrintSet(array);
         AddSum(array,3,6);
         AddSum(array,2,9);
+        Count(array,4);
+        Count(array,9);
+        Count(array,7);
     }
 }
